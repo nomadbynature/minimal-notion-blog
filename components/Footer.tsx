12fe9d87b3a59cb8b0b8a29d11824d76218721c8
nomadbynature <56279371+nomadbynature@users.noreply.github.com>
2022-07-29 19:32:51 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>2022 © {config.author}. <a href="buycoffee.to/nomadbynature">Postaw mi kawę.</a></div>
+      <div className={styles.copyright}>2022 © nomadbynature  <a href="buycoffee.to/nomadbynature">Postaw mi kawę.</a></div>
 
       {hasMounted ? (
         <div className={styles.settings}>
